1665977f700d2c347334da88080062e092dd2071
Oskar Polak <biuro@fivarto.pl>
2018-03-13 19:00:12 +0100

    Initial commit

---
+++ src/Task1.java
@@ -0,0 +1,9 @@
+public class Task1 {
+    public static void main(String[] args) {
+        long someNumber = 12147123123123L;
+        int someNumberTwo = (int) 121111111111111113L;
+
+        System.out.println(someNumber);
+        System.out.println(someNumberTwo);
+    }
+}
---
+++ src/Task2.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Random myRandom = new Random();
+        System.out.println(myRandom.nextInt(100) + 1);
+    }
+}
